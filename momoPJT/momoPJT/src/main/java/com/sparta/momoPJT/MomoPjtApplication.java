f5b3e511bf01f714d5bdcf13ff249d86b0f40e95
정지영 <jiyoung990601@gmail.com>
2024-06-24 08:51:24 +0900

    update_2024_06_23

---
+++ momoPJT/momoPJT/src/main/java/com/sparta/momoPJT/MomoPjtApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.momoPJT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoPjtApplication.class, args);
+	}
+
+}
