6a1758c16af40cb292c1de6d98e77c747220cbb0
정지영 <jiyoung990601@gmail.com>
2024-07-04 23:14:41 +0900

    update_2024_07_04

--- hello-spring/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.*;
+
+// @Entity가 있으면 JPA가 관리해주는 엔티티이다 라는 뜻
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // DB에 있는 column명이 username이면 다음과 같이 매핑한다.
+    // @Column(name = "username")
     private String name;
 
     public Long getId() {
