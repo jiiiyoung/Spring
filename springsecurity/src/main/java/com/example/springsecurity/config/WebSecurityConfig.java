a7ef6fa0278eb337627ad3547208373fda44f8cf
jiyoung <jiyoung990601@gmail.com>
2024-07-22 17:30:26 +0900

    update

---
+++ springsecurity/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.springsecurity.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 스프링 security 지원을 가능하게 하는 애너테이션
+public class WebSecurityConfig { // 스프링 시큐리티를 사용하기 위해 스프링 시큐리티에 필요한 bean을 추가해주는 config 클래스 파일을 추가
+
+    // sprincSEcurity 5.6 부터는 antMatchers 대신 requestMatchers를 사용해야 하고, authorizeRequests는 authorizHttpRequests로 대체
+    // csrf() 설정도 최신 방식으로 변경됨
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+//        http.csrf().disable();
+        http.csrf(csrf -> csrf.disable());
+
+        /*
+        http.authorizeRequests(authorize -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .anyRequest().authenticated());
+        */
+        http.authorizeRequests().anyRequest().authenticated();
+
+        // 로그인 사용
+        http.formLogin(form -> form
+                .loginPage("/api/user/login-page")
+                .permitAll()
+        );
+
+        return http.build();
+    }
+
+}
---
+++ springsecurity/src/main/java/com/example/springsecurity/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.springsecurity.security;
+
+import com.example.springsecurity.entity.User;
+import com.example.springsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(user, user.getUsername(), user.getPassword());
+    }
+}
