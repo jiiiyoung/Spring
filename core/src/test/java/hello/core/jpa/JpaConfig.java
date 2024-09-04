c86b856f3a99cb1f4415d0818254d0f60f834e02
jiyoung <jiyoung990601@gmail.com>
2024-09-04 18:00:06 +0900

    2024-09-04

--- core/src/test/java/hello/core/jpa/JpaConfig.java
+++ core/src/test/java/hello/core/jpa/JpaConfig.java
@@ -1,12 +1,43 @@
 package hello.core.jpa;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "hello.core")
-@EntityScan(basePackages = )
+@EntityScan(basePackages = "hello.core")
 static class JpaConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DriverManagerDataSource("jdbc:h2:mem:testdb", "sa", "");
+    }
 
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("com.example.domain");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        emf.setJpaProperties(properties);
+
+        return emf;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 }
