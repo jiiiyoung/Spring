2fba8822d8c7203cea34b08f2f8790c6e6b73e20
정지영 <jiyoung990601@gmail.com>
2024-09-19 22:33:35 +0900

    feat: 헤더에서 토큰 반í추출하기

--- logintest/src/main/java/com/example/logintest/global/jwt/service/JwtService.java
+++ logintest/src/main/java/com/example/logintest/global/jwt/service/JwtService.java
@@ -1,5 +1,10 @@
 package com.example.logintest.global.jwt.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.logintest.domain.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,14 +36,75 @@ public class JwtService {
 
     private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
     private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+
+    /*
+    Claim: 토큰에서 사용할 정보
+    민감하지 않은 식별을 위한 정보를 담아야한다.
+
+     JWT의 Subject와 Claim으로 email사용
+     우리는 아마 userId, role이지 않을까 싶음
+    */
     private static final String EMAIL_CLAIM = "email";
     private static final String BEARER = "Bearer ";
 
+    private final UserRepository userRepository;
 
     public String createAccessToken(String email) {
         Date now = new Date();
         return JWT.create()
-                .withSubject()
+                .withSubject(ACCESS_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod))
+                .withClaim(EMAIL_CLAIM, email)
+                .sign(Algorithm.HMAC512(secretKey));
     }
+
+    /*
+    * RefreshToken 생성
+    * 리프레시토큰에는 클레임에 정보를 넣지 않을 것이기 때문에 withClaim()은 X
+    * */
+    public String createRefreshToken() {
+        Date now = new Date();
+        return JWT.create()
+                .withSubject(REFRESH_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + refreshTokenExpirationPeriod))
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    /*
+    * AccessToken 헤더에 실어서 보내기
+    * */
+    public void sendAccessToken(HttpServletResponse response, String accessToken){
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.setHeader(accessHeader, accessToken);
+        log.info("재발급된 Access Token : {}", accessToken);
+    }
+
+    /*
+    * AccessToken + RefreshToken 헤더에 실어서 보내기
+    * */
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken){
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken); // response.setHeader
+        setRefreshTokenHeader(response, refreshToken);
+
+        log.info("Access Token, Refresh Token 헤더 설정 완료");
+
+    }
+
+    /*
+    * 헤더에서 RefreshToken 추출
+    * 토큰 형식: Bearer 를 제외하고 순수 토큰만 가져오기 위해서 헤더를 가져온 후 삭제
+    * */
+    public Optional<String> extractRefreshToken(HttpServletRequest request){
+        return Optional.ofNullable(request.getHeader(refreshHeader)) // 요청의 헤더에서
+                .filter(refreshToken -> refreshToken.startsWith(BEARER)) // BEARER로 시작하는 것만 걸러서
+                .map(refreshToken -> refreshToken.replace(BEARER, "")); // BEARER를 제외하고 반환
+    }
+
+
+
+
 }
 
