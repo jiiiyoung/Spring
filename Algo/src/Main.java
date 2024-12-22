97fab94edbf3f327f02bac424352a0886e5cc8a9
정지영 <jiyoung990601@gmail.com>
2024-12-22 15:41:10 +0900

    update

---
+++ Algo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String letter = "--- ------- aaaaaaaa";
+
+        String[] split = letter.split("");
+        printdd();
+    }
+
+    public static void printdd(){
+        System.out.println(1);
+    }
+}
